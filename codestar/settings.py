cdcaad0fbeb5c12beb59980f54860e2cc0e29678
chrisq557 <chrisq557santos@gmail.com>
2025-07-14 10:16:53 +0100

    Install gunicorn package, freeze to requirements.txt, create Procfile file, add herokuapp.com to allowed hostsand change DEBUG to False

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)=h8z6sl!vqbp7v9ld7x5*@j54wsvzdzj1=^!aax37u5iv*0o6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
